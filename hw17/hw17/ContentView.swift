7d29388c3d5b39cb4acca92598e3d7f81df066bf
alexey-z-m <alexey.panda@gmail.com>
2022-08-29 17:09:07 +0300

    project created

---
+++ hw17/hw17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw17
+//
+//  Created by Panda on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw17/hw17/hw17App.swift
@@ -0,0 +1,17 @@
+//
+//  hw17App.swift
+//  hw17
+//
+//  Created by Panda on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct hw17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
