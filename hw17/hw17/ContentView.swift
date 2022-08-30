175cc3a6119a6e84b0dfb460dd8c86419a2e5508
alexey-z-m <alexey.panda@gmail.com>
2022-08-30 17:34:30 +0300

    add tabBar

--- hw17/hw17/ContentView.swift
+++ hw17/hw17/ContentView.swift
@@ -1,21 +1,92 @@
-//
-//  ContentView.swift
-//  hw17
-//
-//  Created by Panda on 29.08.2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom) {
+            TabView {
+                LibraryView()
+                    .tabItem {
+                        Image(systemName: "rectangle.stack.fill")
+                        Text(Strings.media)
+                    }
+                Text("Radio")
+                    .tabItem {
+                        Text(Strings.radio)
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                    }
+                Text("Search")
+                    .tabItem {
+                        Text(Strings.search)
+                        Image(systemName: "magnifyingglass")
+                    }
+            }
+            .accentColor(.red)
+            NowPlayingBar().padding(.bottom, 50)
+        }
+        
     }
+}
+
+struct NowPlayingBar: View {
+    @State var play = false
+    var body: some View {
+        ZStack() {
+            Rectangle()
+                .frame(width: UIScreen.screenWidth, height: 90)
+                .foregroundColor(Color.white.opacity(0.5))
+            .background(.ultraThinMaterial)
+            HStack {
+                Button(action: {}) {
+                    HStack {
+                        Image(systemName: "person")
+                            .resizable()
+                            .frame(width: 45, height: 45)
+                            .shadow(radius: 6)
+                            .padding(.leading, 20)
+                        Text("Taylor Swift")
+                            .padding(.leading, 20)
+                        Spacer()
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                Button(action: {play.toggle()}) {
+                    if play {
+                        Image(systemName: "pause.fill")
+                            .font(.title3)
+                    } else {
+                    Image(systemName: "play.fill")
+                        .font(.title3)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding(.horizontal)
+                Button(action: {}) {
+                    Image(systemName: "forward.fill").font(.title3)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding(.trailing, 30)
+            }
+        }//.padding()
+    }
+}
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    enum Strings {
+        static let media = "Медиатека"
+        static let radio = "Радио"
+        static let search = "Поиск"
+        
     }
 }
--- hw17/hw17/LibraryView.swift
+++ hw17/hw17/LibraryView.swift
@@ -1,15 +1,95 @@
-//
-//  LibraryView.swift
-//  hw17
-//
-//  Created by Panda on 30.08.2022.
-//
-
 import SwiftUI
 
 struct LibraryView: View {
+    @State var editLibrary = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            if editLibrary {
+                EditLibrary()
+                    .toolbar {
+                        NavigationLink {} label: {
+                            Button(action: {
+                                editLibrary.toggle()
+                                
+                            }) {
+                                Text("Готово")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+            } else {
+                MainLibrary()
+                    .toolbar {
+                        NavigationLink {} label: {
+                            Button(action: {
+                                editLibrary.toggle()
+                            }) {
+                                Text("Править")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct MainLibrary: View {
+    var body: some View {
+        VStack {
+            Text("Ищите свою музыку?")
+                .bold()
+                .font(.system(size: 31))
+                .navigationTitle("Медиатека")
+            Text("Здесь появится купленная Вами в iTunes Store музыка")
+                .font(.system(size: 23))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct EditLibrary: View {
+    @State var selectedItems = Set<UUID>()
+    let libraryItems = ["1","2","3","4","5","6","7","8","9","10","11"]
+    @State var selections: [String] = []
+    var body: some View {
+        List(selection: $selectedItems) {
+            ForEach(libraryItems, id: \.self) { item in
+                MultipleSelectionRow(title: item, isSelected: selections.contains(item)) {
+                    if self.selections.contains(item) {
+                        self.selections.removeAll(where: { $0 == item })
+                    }
+                    else {
+                        self.selections.append(item)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Медиатека")
+    }
+}
+
+struct MultipleSelectionRow: View {
+    var title: String
+    var isSelected: Bool
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: self.action) {
+            HStack {
+                if self.isSelected {
+                    Text(self.title)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Image(systemName: "checkmark")
+                        .listRowBackground(Color.red)
+                } else {
+                    Text(self.title)
+                        .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
@@ -17,4 +97,10 @@ struct LibraryView_Previews: PreviewProvider {
     static var previews: some View {
         LibraryView()
     }
+}
+
+extension UIScreen{
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
 }
