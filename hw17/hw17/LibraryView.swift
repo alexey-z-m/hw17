fb7fecd81fbdfaec739fc7ffbe8b11d5a38780da
alexey-z-m <alexey.panda@gmail.com>
2022-08-30 17:33:57 +0300

    add playBar

---
+++ hw17/hw17/LibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryView.swift
+//  hw17
+//
+//  Created by Panda on 30.08.2022.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
